db6f22bdf3e975587b54e268c988d8ee80e30af1
avraam112russo <avraam112russo@gmail.com>
2024-02-27 22:35:57 +0300

    commit

--- src/main/java/com/example/ejbasynchronusex/ProgramWorkSchedule.java
+++ src/main/java/com/example/ejbasynchronusex/ProgramWorkSchedule.java
@@ -2,6 +2,8 @@ package com.example.ejbasynchronusex;
 
 import jakarta.annotation.Resource;
 import jakarta.ejb.*;
+import jakarta.interceptor.AroundTimeout;
+import jakarta.interceptor.InvocationContext;
 
 @Singleton
 public class ProgramWorkSchedule {
@@ -14,8 +16,8 @@ public class ProgramWorkSchedule {
     private SessionContext sessionContext;
     public void example(){
         TimerService timerService = sessionContext.getTimerService();
-//        ScheduleExpression scheduleExpression = new ScheduleExpression().hour("*").minute("*").second("*");
-        ScheduleExpression scheduleExpression = new ScheduleExpression().hour("22").minute("25").second("45");
+        ScheduleExpression scheduleExpression = new ScheduleExpression().hour("*").minute("*").second("*");
+//        ScheduleExpression scheduleExpression = new ScheduleExpression().hour("22").minute("25").second("45");
         timerService.createCalendarTimer(scheduleExpression, new TimerConfig("String from timer config", false));
         //timer service automatic search methods which mark as @Timeout and running him
     }
@@ -23,5 +25,10 @@ public class ProgramWorkSchedule {
     public void workToTimer(Timer timer){
         System.out.println("Time to work");
         System.out.println(timer.getInfo());
+    }
+    @AroundTimeout
+    Object aroundTimeOutMethod(InvocationContext invocationContext) throws Exception {
+        System.out.println("Around timeout method work...");
+        return invocationContext.proceed();
     }
 }
